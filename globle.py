acb47d558531157975eb8077bc6583213b7d5a2f
Nicholas <nicholaslk@al.insper.edu.br>
2022-05-09 17:10:42 -0300

    normaliza funcao

--- globle.py
+++ globle.py
@@ -1,3 +1,10 @@
 #primeira funcao globle
-print(1+1)
-print(5+5)
+def normaliza(continentes):
+    normalizada = {}
+    for continente, pais in continentes.items():
+        try:
+            for keys in pais.keys():
+                normalizada[keys]=pais[keys]
+                normalizada[keys]['continue'] = continente
+        except: pass
+    return normalizada
