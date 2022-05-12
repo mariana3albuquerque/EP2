6bf7705389fef5d20ae2a0d488d44567c2b71e00
mariana3albuquerque <91220409+mariana3albuquerque@users.noreply.github.com>
2022-05-12 19:50:18 -0300

    njng

---
+++ distanciadehaversine.py
@@ -0,0 +1,4 @@
+import math
+def haversine(r,phi1,lambda1,phi2,lambda2):
+    dentro_raiz = (math.sin(math.radians((phi2-phi1)/2)))*2 + (math.cos(math.radians(phi1))*math.cos(math.radians(phi2))(math.sin(math.radians((lambda2-lambda1)/2)))**2)
+    return 2*r*math.asin(math.sqrt(dentro_raiz))
