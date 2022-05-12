2379417888962f53b308ba75cd49a71675ee1bd0
mariana3albuquerque <91220409+mariana3albuquerque@users.noreply.github.com>
2022-05-12 19:43:20 -0300

    funcao

---
+++ normaliza.py
@@ -0,0 +1,9 @@
+def normaliza(continentes):
+    normalizada = {}
+    for continente, pais in continentes.items():
+        try:
+            for keys in pais.keys():
+                normalizada[keys]=pais[keys]
+                normalizada[keys]['continue'] = continente
+        except: pass
+    return normalizada
