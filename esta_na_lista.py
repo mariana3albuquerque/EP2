6537da5387f0a79bfd81ded2562865f4221e5550
mariana3albuquerque <91220409+mariana3albuquerque@users.noreply.github.com>
2022-05-15 14:02:08 -0300

    resto do que eu fiz

--- esta_na_lista.py
+++ esta_na_lista.py
@@ -1,3 +1,4 @@
 def esta_na_lista(pais, lista):
-    if pais in [pais[0] for pais in lista]: return True
+    if pais in [pais[0] for pais in lista]: 
+        return True
     return False
--- sorteia_letra com_restricao.py
+++ sorteia_letra com_restricao.py
@@ -1,10 +1,13 @@
-import random 
-def sorteia_letra(s, l):
-    ac = ['.', ',', '-', ';', ' ']
-    s = s.lower()
-    for e in (ac + l):
-        s = s.replace(e, '')
-    if len(s) == 0:
-        return ''
-    return random.choice(s)
-    
+import random
+
+def sorteia_letra(palavra,lista):
+    especial = ['.', ',', '-', ';', ' ']
+    final = []
+    p = palavra.lower()
+    for i in p:
+        if i not in especial and i not in lista and i not in final:
+            final.append(i)
+    if final == []:
+        return ""
+    else:
+        return (random.choice(final))
